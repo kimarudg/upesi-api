c8fb305653d511ed187c130d66102dcd7d77337b
David Kimaru <kimarudg@gmail.com>
2022-07-18 07:06:16 +0300

    Add deposit and credit features

--- src/bank-account/services/bank-account-statement/bank-account-statement.service.ts
+++ src/bank-account/services/bank-account-statement/bank-account-statement.service.ts
@@ -1,4 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import { BankAccountStatementModel } from '@app/bank-account/models/bank-account-transaction.model';
+import { BankAccountModel } from '@app/bank-account/models/bank-account.model';
+import { BankAccountService } from '@app/bank-account/services/bank-account/bank-account.service';
+import { inTransaction } from '@app/core/modules/database/';
+import { TYPES } from '@app/types';
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { EntityManager } from 'typeorm';
+import { UserModel } from './../../../core/modules/user/models/user.model';
+import { BankAccountRepository } from './../../repositories/bank-account.repository';
 
 @Injectable()
-export class BankAccountStatementService {}
+export class BankAccountStatementService {
+  async debitAccount(accountId: string, amount: number, editor: UserModel) {
+    return inTransaction(this.manager, async (manager) => {
+      const bankAccount = await manager
+        .findOneOrFail(BankAccountModel, accountId)
+        .catch(() => {
+          throw new NotFoundException(
+            `Account with id:${accountId} not found!`,
+          );
+        });
+      bankAccount.currentBalance = (bankAccount.currentBalance || 0) + amount;
+      bankAccount.lastUpdatedBy = editor;
+      manager.save(BankAccountModel, bankAccount);
+
+      // create statement
+      const statement = Object.assign(new BankAccountStatementModel(), {
+        account: bankAccount,
+        debit: amount,
+        createdBy: editor,
+      });
+      return manager.save(BankAccountStatementModel, statement);
+    });
+  }
+
+  async withdrawAccount(accountId: string, amount: number, editor: UserModel) {
+    return inTransaction(this.accountRepository.manager, async (manager) => {
+      const bankAccount = await manager
+        .findOneOrFail(BankAccountModel, accountId)
+        .catch(() => {
+          throw new NotFoundException(
+            `Account with id:${accountId} not found!`,
+          );
+        });
+
+      const resultingBalance = (bankAccount.currentBalance || 0) - amount;
+      if (resultingBalance < 0) {
+        throw new Error(
+          `The account does not have enough balance to withdraw ${amount}`,
+        );
+      }
+      bankAccount.currentBalance = resultingBalance;
+      bankAccount.lastUpdatedBy = editor;
+      manager.save(BankAccountModel, bankAccount);
+
+      // create statement
+      const statement = Object.assign(new BankAccountStatementModel(), {
+        account: bankAccount,
+        credit: amount,
+        createdBy: editor,
+      });
+      return manager.save(BankAccountStatementModel, statement);
+    });
+  }
+
+  constructor(
+    @Inject(TYPES.BankAccountRepository)
+    private accountRepository: BankAccountRepository,
+    @Inject(TYPES.EntityManager)
+    private readonly manager: EntityManager,
+  ) {}
+}
