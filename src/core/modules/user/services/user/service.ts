ac2e4815c854eb7ffb75d0186d7f4835badd303a
David Kimaru <kimarudg@gmail.com>
2022-07-20 23:12:54 +0300

    Allow self registration

--- src/core/modules/user/services/user/service.ts
+++ src/core/modules/user/services/user/service.ts
@@ -96,7 +96,7 @@ export class UserService {
       phone,
       passwordHash,
       needPasswordChange: false,
-      confirmed: false,
+      confirmed: true,
       active: true,
       profile,
     };
